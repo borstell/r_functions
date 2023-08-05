566804cc8ec000470eca5f9407efa0c8dc289646
Carl Börstell <calleborstell@gmail.com>
2023-08-05 22:14:04 +0200

    Add files via upload

--- segment_elan_turns.R
+++ segment_elan_turns.R
@@ -1,5 +1,7 @@
 # Load libraries
 library(tidyverse)
+library(slider)
+library(glue)
 
 # Set themes
 theme_set(theme_classic(base_size=15))
@@ -8,16 +10,20 @@ options(ggplot2.discrete.colour = c("dodgerblue", "orange"))
 # Make example data
 df <- tibble(file=paste0("file_00", c(rep(1, 14), rep(2, 6))),
              signer=paste0("S",c(rep(1, 6), 2, 1, 2, 2, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1)),
-             hand=c("right", "right", "right", "left", "right", "right", "right", "right", "right", "left", "left", "right", "right", "right", "right", "left", "right", "right", "right", "left"),
+             hand=c("rh", "rh", "rh", "lh", "rh", "rh", "rh", "rh", "rh", "lh", "lh", "rh", "rh", "rh", "rh", "lh", "rh", "rh", "rh", "lh"),
              start=c(1000, 1200, 1400, 1600, 1650, 1850, 2000, 2500, 2550, 2600, 2650, 2800, 2900, 3400, 1000, 1100, 2000, 2050, 2500, 2550),
              end=c(1150, 1350, 1550, 2550, 1800, 2000, 2100, 2650, 2600, 2650, 2700, 2900, 3100, 3500, 1200, 1900, 2100, 2150, 2600, 2800),
              annotation=paste0("GLOSS-", LETTERS[1:20])) 
 
+t <- df %>% 
+  mutate(ci = consecutive_id(signer), .by = file) %>% 
+  mutate(rn = row_number(), .by = c(file, signer))
+
 # Define function for strictly sequential turn segments (chronological by start time)
-turn_seq <- function(data, file, start, end, participant, annotation, simplify = TRUE) {
+turn_seq <- function(data, file, start, end, participant, annotation, simplify = TRUE, long = FALSE) {
   df <- data %>% 
     arrange({{ file }}, {{ start }}) %>% 
-    mutate(turn = consecutive_id({{ file }}, {{ participant }}))
+    mutate(turn = consecutive_id({{ participant }}), .by = {{ file }})
   if (simplify) {
     df <- df %>% 
       group_by({{ file }}, {{ participant}}, turn) %>% 
@@ -28,9 +34,21 @@ turn_seq <- function(data, file, start, end, participant, annotation, simplify =
       slice(1) %>% 
       select(-{{ annotation }}) %>% 
       ungroup()
+    if (long) {
+      df <- df %>% 
+        arrange({{ file }}, {{ participant }}, {{ start }}) %>% 
+        mutate(turn = if_else(row_number()!=1 & start<=lag(end), lag(turn), turn), .by = c({{ file }}, {{ participant }})) %>% 
+        group_by({{ file }}, {{ participant}}, turn) %>%
+        mutate(annotations = paste0(annotations, collapse = " ")) %>% 
+        mutate(start = min({{ start }}),
+               end = max({{ end }})) %>%
+        slice(1) %>% 
+        ungroup()
+    }
   }
   return(df)
 }
+
 
 # Define function for turn segments within intervals (chronological by start time per participant, but merge segments within interval)
 turn_interval <- function(data,  file, start, end, participant, annotation, interval = 300, simplify = TRUE) {
@@ -52,31 +70,79 @@ turn_interval <- function(data,  file, start, end, participant, annotation, inte
   return(df)
 }
 
+
+# Define function for turn segments by most signs per 3-signs moving time window
+turn_quant <- function(data,  file, start, end, participant, annotation, simplify = TRUE, long = FALSE) {
+  
+  # Create windows of "whose turn" by max signing (number of signs)
+  df <- data %>% 
+    arrange({{ file }}, {{ start }}) %>% 
+    mutate(whose_turn = slider::slide_chr(signer, ~ names(which.max(table(.x))), .before = 1, .after = 1)) %>% 
+    mutate(current_turn = if_else({{ participant }}==whose_turn, "same", "other")) %>% 
+    mutate(turn = consecutive_id(whose_turn))
+  if (simplify) {
+    df <- df %>% 
+      group_by({{ file }}, turn) %>% 
+      mutate(annotations = paste0({{ annotation }}, collapse = " ")) %>% 
+      mutate(start = min({{ start }}),
+             end = max({{ end }})) %>%
+      slice(1) %>% 
+      select(-c({{ annotation }})) %>% 
+      ungroup()
+    if (long) {
+      df <- df %>% 
+        arrange({{ file }}, {{ participant }}, {{ start }}) %>% 
+        mutate(turn = if_else(row_number()!=1 & start<=lag(end), lag(turn), turn), .by = c({{ file }}, {{ participant }})) %>% 
+        group_by({{ file }}, {{ participant}}, turn) %>%
+        mutate(annotations = paste0(annotations, collapse = " ")) %>% 
+        mutate(start = min({{ start }}),
+               end = max({{ end }})) %>%
+        slice(1) %>% 
+        ungroup()
+    }
+  }
+  return(df)
+}
+
+
 # Plot sequential (gives overlapping turns within participants in this example)
 ggplot() +
-  geom_segment(data=turn_seq(df, file = file, start = start, end = end, participant = signer, annotation = annotation, simplify = T),
-               aes(x=start, xend=end, y=paste0(signer,"_seq"), yend=paste0(signer, "_seq"), color=signer),
+  geom_segment(data=turn_seq(df, file = file, start = start, end = end, participant = signer, annotation = annotation, simplify = T, long = T),
+               aes(x=start, xend=end, y=glue::glue("{signer}_(seq)"), yend=glue::glue("{signer}_(seq)"), color=signer),
                linewidth=5, show.legend = F, alpha=.5) +
-  geom_segment(data=df, aes(x=start, xend=end, y=paste0(signer, "_", hand), yend=paste0(signer, "_", hand), color=signer),
+  geom_segment(data=df, aes(x=start, xend=end, y=glue::glue("{signer}_{hand}"), yend=glue::glue("{signer}_{hand}"), color=signer),
                linewidth=5, show.legend = F, alpha=1) +
   labs(y="", x="Time (ms)") +
   facet_wrap(~file, ncol=1)
+#ggsave(width = 7, height = 4, units = "in", dpi = 600)
+
 
 # Plot with different intervals
+
+# Set intervals in ms
+#ms <- 100
+#ms <- 200
+ms <- 300
+#ms <- 800 
+
 ggplot() +
-  geom_segment(data=turn_interval(df, file = file, start = start, end = end, participant = signer, annotation = annotation, interval = 100, simplify = T),
-               aes(x=start, xend=end, y=paste0(signer,"_100"), yend=paste0(signer, "_100"), color=signer),
+  geom_segment(data=turn_interval(df, file = file, start = start, end = end, participant = signer, annotation = annotation, interval = ms, simplify = T),
+               aes(x=start, xend=end, y=glue::glue("{signer}_(interval: {ms})"), yend=glue::glue("{signer}_(interval: {ms})"), color=signer),
                linewidth=5, show.legend = F, alpha=.5) +
-  geom_segment(data=turn_interval(df, file = file, start = start, end = end, participant = signer, annotation = annotation, interval = 200, simplify = T),
-               aes(x=start, xend=end, y=paste0(signer,"_200"), yend=paste0(signer, "_200"), color=signer),
-               linewidth=5, show.legend = F, alpha=.5) +
-  geom_segment(data=turn_interval(df, file = file, start = start, end = end, participant = signer, annotation = annotation, interval = 400, simplify = T),
-               aes(x=start, xend=end, y=paste0(signer,"_400"), yend=paste0(signer, "_400"), color=signer),
-               linewidth=5, show.legend = F, alpha=.5) +
-  geom_segment(data=turn_interval(df, file = file, start = start, end = end, participant = signer, annotation = annotation, interval = 800, simplify = T),
-               aes(x=start, xend=end, y=paste0(signer,"_800"), yend=paste0(signer, "_800"), color=signer),
-               linewidth=5, show.legend = F, alpha=.5) +
-  geom_segment(data=df, aes(x=start, xend=end, y=paste0(signer, "_", hand), yend=paste0(signer, "_", hand), color=signer),
+  geom_segment(data=df, aes(x=start, xend=end, y=glue::glue("{signer}_{hand}"), yend=glue::glue("{signer}_{hand}"), color=signer),
                linewidth=5, show.legend = F, alpha=1) +
   labs(y="", x="Time (ms)") +
   facet_wrap(~file, ncol=1)
+#ggsave(width = 7, height = 4, units = "in", dpi = 600)
+
+
+# Plot quantified
+ggplot() +
+  geom_segment(data=turn_quant(df, file = file, start = start, end = end, participant = signer, annotation = annotation, simplify = T, long = T),
+               aes(x=start, xend=end, y=glue::glue("{signer}_(quant)"), yend=glue::glue("{signer}_(quant)"), color=signer),
+               linewidth=5, show.legend = F, alpha=.5) +
+  geom_segment(data=df, aes(x=start, xend=end, y=glue::glue("{signer}_{hand}"), yend=glue::glue("{signer}_{hand}"), color=signer),
+               linewidth=5, show.legend = F, alpha=1) +
+  labs(y="", x="Time (ms)") +
+  facet_wrap(~file, ncol=1)
+#ggsave(width = 7, height = 4, units = "in", dpi = 600)
